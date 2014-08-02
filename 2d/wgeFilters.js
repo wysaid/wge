c39669dc9535653fa6e7a73bce330d91d87dff68
wysaid <admin@wysaid.org>
2014-08-02 18:07:13 +0800

    添加资源并完善demo1

---
+++ 2d/wgeFilters.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+* wgeFilters.js
+*
+*  Created on: 2014-8-2
+*      Author: Wang Yang
+*        Blog: http://blog.wysaid.org
+*        Mail: admin@wysaid.org
+*/
+
+/*
+	简介： 提供在context-2d环境下的简单CPU滤镜
+*/
+
+WGE.Filters = WGE.Class(
+{
+	
+});
