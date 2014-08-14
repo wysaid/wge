1894b106ff5b371e1f3b92338654795d0c4d7830
wangyang <admin@wysaid.org>
2014-08-14 16:12:27 +0800

    修改重入时检测是否已经保存好了黑白图像。

--- 2d/demo_photoFrames/photoFrames.js
+++ 2d/demo_photoFrames/photoFrames.js
@@ -395,8 +395,10 @@ FTPhotoFrame.initScene = function(imageArray, w, h, globalZ, timeStamp, stillTim
 	//0:5:25~0:8:20 [0, 2800]
 	{
 		var img = WGE.rotateArray(imageArray);
+		var imgBW = typeof img.PhotoFrameBW == 'object' ? img.PhotoFrameBW : (img.PhotoFrameBW = filterBW.bind(img).run());
+		
 		var frame1 = S(0, 2800, img, -1);
-		var frame2 = S(1000, 2800, filterBW.bind(img).run(), -1);
+		var frame2 = S(1000, 2800, imgBW, -1);
 
 		frame1.zIndex = globalZ + 1;
 		frame2.zIndex = globalZ;
@@ -475,8 +477,10 @@ FTPhotoFrame.initScene = function(imageArray, w, h, globalZ, timeStamp, stillTim
 
 		////////////////////////////////////////////////
 		var img1 = WGE.rotateArray(imageArray);
+		var img1BW = typeof img1.PhotoFrameBW == 'object' ? img1.PhotoFrameBW : (img1.PhotoFrameBW = filterBW.bind(img1).run());
+
 		var frame3 = S(3133, 6000, img1, -1);
-		var frame4 = S(3133, 6000, filterBW.bind(img1).run(), -1);
+		var frame4 = S(3133, 6000, img1BW, -1);
 		frame3.zIndex = globalZ;
 		frame4.zIndex = globalZ + 1;
 		frame3.setHotspotWithRatio(0.5, 0.1);
@@ -493,7 +497,9 @@ FTPhotoFrame.initScene = function(imageArray, w, h, globalZ, timeStamp, stillTim
 		//////////////////////////////////////////////////
 
 		var img2 = WGE.rotateArray(imageArray);
-		var frame5 = S(4133, 6000, filterBW.bind(img2).run(), -1);
+		var img2BW = typeof img2.PhotoFrameBW == 'object' ? img2.PhotoFrameBW : (img2.PhotoFrameBW = filterBW.bind(img2).run());
+
+		var frame5 = S(4133, 6000, img2BW, -1);
 		var frame6 = S(4133, 6000, img2, -1);
 		frame5.zIndex = globalZ - 2;
 		frame6.zIndex = globalZ - 1;
@@ -618,7 +624,10 @@ FTPhotoFrame.initScene = function(imageArray, w, h, globalZ, timeStamp, stillTim
 	//0:14:22 
 	{
 		var frame1 = frames.frame1;
-		var frame2 = S(9100, 15000, filterBW.bind(frame1.img).run(), -1);
+
+		var frame1ImgBW = typeof frame1.img.PhotoFrameBW == 'object' ? frame1.img.PhotoFrameBW : (frame1.img.PhotoFrameBW = filterBW.bind(frame1.img).run());
+
+		var frame2 = S(9100, 15000, frame1ImgBW, -1);
 		frame2.setHotspotWithRatio(0.5, 0.1);
 		frame2.moveTo(w / 2 - 10, h / 10);
 		frame2.zIndex = globalZ;
@@ -711,7 +720,10 @@ FTPhotoFrame.initScene = function(imageArray, w, h, globalZ, timeStamp, stillTim
 		else if(stillTime < 15000)
 			stillTime = 15000;
 
-		var frame5 = S(14000, stillTime, filterBW.bind(img4).run(), -1);
+
+		var img4BW = typeof img4.PhotoFrameBW == 'object' ? img4.PhotoFrameBW : (img4.PhotoFrameBW = filterBW.bind(img4).run());
+
+		var frame5 = S(14000, stillTime, img4BW, -1);
 		frame5.setHotspotWithRatio(0.5, 0.1);
 		frame5.moveTo(w / 2, h / 10);
 		frame5.zIndex = frame3.zIndex - 0.5;
