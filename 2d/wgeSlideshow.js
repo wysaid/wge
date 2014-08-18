a41f428a063db03baf22645aa301e4864cd8d2b2
wangyang <admin@wysaid.org>
2014-08-18 14:24:35 +0800

    update

---
+++ 2d/wgeSlideshow.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+* wgeSlideshow.js
+*
+*  Created on: 2014-8-14
+*      Author: Wang Yang
+*        Blog: http://blog.wysaid.org
+*/
+
+/*
+
+
+
+
+*/
+
+
+WGE.SlideshowInterface = WGE.Class(
+{
+
+});
