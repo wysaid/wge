037d1245f5e31398e2143ebddde5c21dcfc49516
ixshells <ixshells@163.com>
2014-08-27 20:58:07 +0800

    添加移动动作

--- 2d/slideshows/Vignette.js
+++ 2d/slideshows/Vignette.js
@@ -37,13 +37,21 @@ WGE.Vignette = WGE.Class(WGE.SlideshowInterface,
         sprite.setHotspot2Center();
 
         var sprite1 = new mySprite(0, 8000, this.srcImages[1], -1);
-        sprite1.moveTo(-sprite1.size.data[0]/2, WGE.SlideshowSettings.height/2);
+        sprite1.moveTo(WGE.SlideshowSettings.width/2 - sprite1.size.data[0], WGE.SlideshowSettings.height/2);
         sprite1.setHotspot2Center();
 
         var action = new WGE.Actions.acceleratedMoveAction([0, 4000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 
             [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
         
+        var action1 = new  WGE.Actions.acceleratedMoveAction([0, 4000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 
+            [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
+
+
+        var action2 = new WGE.Actions.UniformLinearMoveAction([4000, 5000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], [WGE.SlideshowSettings.width/2+sprite1.size.data[0], WGE.SlideshowSettings.height/2], 1);
+        var action3 = new WGE.Actions.UniformLinearMoveAction([4000, 5000], [WGE.SlideshowSettings.width/2 - sprite1.size.data[0], WGE.SlideshowSettings.height/2], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
         sprite.push(action);
+        sprite.push(action2);
+        sprite1.push(action3);
 
         this.timeline.pushArr([sprite, sprite1]);
         this.timeline.start(0);
