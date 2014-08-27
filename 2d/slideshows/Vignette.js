9277369a969adc59fc427932259670b22876c865
ixshells <ixshells@163.com>
2014-08-27 22:21:31 +0800

    代码修改

--- 2d/slideshows/Vignette.js
+++ 2d/slideshows/Vignette.js
@@ -32,25 +32,31 @@ WGE.Vignette = WGE.Class(WGE.SlideshowInterface,
     initTimeline : function(config)
     {
         this.timeline = new WGE.TimeLine(50000);
-        var sprite = new mySprite(0, 5000, this.srcImages[0], -1);
-        sprite.moveTo(WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2);
+        this.ControlActions();
+	},
+
+
+	ControlActions : function()
+	{
+		var sprite = new mySprite(0, 5000, this.srcImages[0], -1);
+        //sprite.moveTo(WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2);
         sprite.setHotspot2Center();
 
         var sprite1 = new mySprite(4000, 9000, this.srcImages[1], -1);
-        sprite1.moveTo(WGE.SlideshowSettings.width/2 - sprite1.size.data[0], WGE.SlideshowSettings.height/2);
+        //sprite1.moveTo(WGE.SlideshowSettings.width/2 - sprite1.size.data[0], WGE.SlideshowSettings.height/2);
         sprite1.setHotspot2Center();
         var action = new WGE.Actions.acceleratedMoveAction([0, 4000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 
             [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
         
         var action1 = new  WGE.Actions.acceleratedMoveAction([0, 4000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 
             [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
-
-
+        
         var action2 = new WGE.Actions.UniformLinearMoveAction([4000, 5000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], [WGE.SlideshowSettings.width/2+sprite1.size.data[0], WGE.SlideshowSettings.height/2], 1);
         var action3 = new WGE.Actions.UniformLinearMoveAction([0, 1000], [WGE.SlideshowSettings.width/2 - sprite1.size.data[0], WGE.SlideshowSettings.height/2], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
+       
         var action4 = new WGE.Actions.acceleratedMoveAction([1000, 5000], [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 
-	    
 	    [WGE.SlideshowSettings.width/2, WGE.SlideshowSettings.height/2], 1);
+        
         sprite.push(action);
         sprite.push(action2);
 
@@ -59,8 +65,9 @@ WGE.Vignette = WGE.Class(WGE.SlideshowInterface,
 
         this.timeline.pushArr([sprite, sprite1]);
         this.timeline.start(0);
-
 	},
+
+
 
 
 });
